c95a5cc0c5ebce5edd7ecc9c88342b63a5597cc1
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-11-08 16:51:46 -0600

    ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,99 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# food = Food.find_by(id:1)
+# food.image_url="/images/potato-chips-cure-cramps.jpg"
+# food.save
+# food = Food.find_by(id:2)
+# food.image_url="/images/images.jpg"
+# food.save
+
+# food = Food.find_by(id:3)
+# food.image_url="/images/pasta.jpg"
+# food.save
+
+ # food = Food.find_by(id:4)
+ # food.image_url="/images/icecream.jpg"
+ # food.save
+
+ # food = Food.find_by(id:5)
+ # food.image_url="/images/chocolatecake.jpg"
+ # food.save
+
+ # food = Food.find_by(id:6)
+ # food.image_url="/images/kalechips.jpg"
+ # food.save
+
+ # food = Food.find_by(id:7)
+ # food.image_url="/images/zucchinichip.jpg"
+ # food.save
+
+ # food = Food.find_by(id:8)
+ # food.image_url="/images/sweetpotatochips.jpg"
+ # food.save
+
+ # food = Food.find_by(id:9)
+ # food.image_url="/images/bakedbeetchips.jpg"
+ # food.save
+
+ # food = Food.find_by(id:10)
+ # food.image_url="/images/oatmealcookies.jpg"
+ # food.save
+
+ # food = Food.find_by(id:11)
+ # food.image_url="/images/almondcookies.jpg"
+ # food.save
+
+ # food = Food.find_by(id:12)
+ # food.image_url="/images/buckwheatcookies.jpg"
+ # food.save
+
+ # food = Food.find_by(id:13)
+ # food.image_url="/images/granolacookies.jpg"
+ # food.save
+
+ # food = Food.find_by(id:14)
+ # food.image_url="/images/chickpeapasta.jpg"
+ # food.save
+
+ # food = Food.find_by(id:15)
+ # food.image_url="/images/quinoapasta.jpg"
+ # food.save
+
+
+ # food = Food.find_by(id:16)
+ # food.image_url="/images/brownricepasta.jpg"
+ # food.save
+
+ # food = Food.find_by(id:17)
+ # food.image_url="/images/cornpasta.jpg"
+ # food.save
+
+ # food = Food.find_by(id:18)
+ # food.image_url="/images/nonfatfrozenyogurt.jpg"
+ # food.save
+
+ # food = Food.find_by(id:19)
+ # food.image_url="/images/frozenbananas.jpg"
+ # food.save
+
+ # food = Food.find_by(id:20)
+ # food.image_url="/images/coconutgranita.jpg"
+ # food.save
+
+ # food = Food.find_by(id:21)
+ # food.image_url="/images/almondmilkpopsicle.jpg"
+ # food.save
+
+ # food = Food.find_by(id:22)
+ # food.image_url="/images/carrotcake.jpg"
+ # food.save
+
+ # food = Food.find_by(id:23)
+ # food.image_url="/images/frozenbananas.jpg"
+ # food.save
+
+ # food = Food.find_by(id:24)
+ # food.image_url="/images/pumpkincake.jpg"
+ # food.save
+
+ # food = Food.find_by(id:25)
+ # food.image_url="/images/bananacake.jpg"
+ # food.save
